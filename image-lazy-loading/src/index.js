d22a68a11b0f177e239e84b258c6e3b33d819785
partykyoung <partykyoung@gmail.com>
2024-08-15 00:19:02 +0900

    feat: 성능이슈 있는 모자이크 함수 제거

--- image-lazy-loading/src/index.js
+++ image-lazy-loading/src/index.js
@@ -80,37 +80,6 @@ async function generateMosaic(img) {
     });
   };
 
-  // const mosaic2 = (blockSize) => {
-  //   return new Promise((resolve) => {
-  //     const width = canvas.width;
-  //     const height = canvas.height;
-  //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-  //     for (let y = 0; y < height; y += blockSize) {
-  //       for (let x = 0; x < width; x += blockSize) {
-  //         // 각 블록의 색상을 추출
-  //         const imageData = ctx.getImageData(x, y, blockSize, blockSize);
-  //         let red = 0,
-  //           green = 0,
-  //           blue = 0;
-  //         for (let i = 0; i < imageData.data.length; i += 4) {
-  //           red += imageData.data[i];
-  //           green += imageData.data[i + 1];
-  //           blue += imageData.data[i + 2];
-  //         }
-  //         const pixelCount = imageData.data.length / 4;
-  //         red = Math.floor(red / pixelCount);
-  //         green = Math.floor(green / pixelCount);
-  //         blue = Math.floor(blue / pixelCount);
-  //         // 추출한 색상으로 블록을 채움
-  //         ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
-  //         ctx.fillRect(x, y, blockSize, blockSize);
-  //       }
-  //     }
-  //     resolve();
-  //   });
-  // };
-
   canvas.style.width = "100%";
   canvas.style.height = "100%";
   canvas.style.position = "absolute";
@@ -123,29 +92,30 @@ async function generateMosaic(img) {
   const ITERATION_DELAY = 100;
   const delay = (ms) => new Promise((res) => setTimeout(res, ms));
   await mosaic1(8);
-  // await mosaic2(80);
+
   await delay(ITERATION_DELAY);
   await mosaic1(16);
-  // await mosaic2(60);
+
   await delay(ITERATION_DELAY);
   await mosaic1(32);
-  // await mosaic2(40);
+
   await delay(ITERATION_DELAY);
   await mosaic1(48);
-  // await mosaic2(30);
+
   await delay(ITERATION_DELAY);
   await mosaic1(96);
-  // await mosaic2(20);
+
   await delay(ITERATION_DELAY);
   await mosaic1(128);
-  // await mosaic2(15);
+
   await delay(ITERATION_DELAY);
   await mosaic1(250);
-  // await mosaic2(10);
+
   await delay(ITERATION_DELAY);
   await mosaic1(512);
-  // await mosaic2(5);
+
   await delay(ITERATION_DELAY);
+
   img.parentNode.removeChild(canvas);
 }
 
